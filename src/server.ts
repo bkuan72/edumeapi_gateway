8ae44474b90f07d83951a20c3bcbc9d7028dfb57
Brian Kuan <bkuan72@gmail.com>
2021-11-22 16:20:49 +1300

    moved cron tasks to own microservice

--- src/server.ts
+++ src/server.ts
@@ -12,29 +12,6 @@ import appDbConnection from './modules/AppDBModule';
 SysEnv.init();
 validateEnv();
 
-// const blacklistTokens = new TokenModel(blacklist_tokens_schema_table);
-
-// const tokens = new TokenModel(tokens_schema_table);
-
-
-
-// SysLog.info('Cron setup to purge expired blacklistTokens every minute')
-
-// const cronTasks: cron.ScheduledTask[] = [
-//   cron.schedule('* 3 5 * * *', () => {
-//     SysLog.info('cron run at 5.03am to purge expired blacklist token');
-//     blacklistTokens.purgeExpired();
-//   }),
-//   cron.schedule('* */15 * * * * *', () => {
-//     // SysLog.info('cron run every 15 minutes to purge expired tokens');
-//     tokens.purgeExpired();
-//   })
-// ];
-
-// cronTasks.forEach((task) => {
-//   task.start();
-// });
-
 
 const expressApp = new App (SysEnv.PORT);
 // insert  authentication controller into stack
